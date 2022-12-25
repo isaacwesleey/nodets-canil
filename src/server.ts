d834386afa9c61ae846a3984fbece31045143102
Isaac Wesley <iwesleypsantos@gmail.com>
2022-12-25 18:21:54 +0100

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.send('404 - Page not found')
+    res.render('pages/404');
     });
 
 server.listen(process.env.PORT, () => {
